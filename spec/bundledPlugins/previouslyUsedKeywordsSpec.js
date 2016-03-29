b1b77fc41d659c70121c6ed9ec04d334a687ad0c
terw-dan <danny@yoast.com>
2016-03-29 17:04:06 +0200

    include specs

--- spec/bundledPlugins/previouslyUsedKeywordsSpec.js
+++ spec/bundledPlugins/previouslyUsedKeywordsSpec.js
@@ -7,16 +7,21 @@ var Factory = require( "../helpers/factory.js" );
 var i18n = Factory.buildJed();
 
 var app = {
-	i18n: i18n,
 	paper: new Paper( "text", { keyword: "keyword"} )
 };
 
 var searchUrl =  "http://search?{keyword}";
 var postUrl = "http://post?{id}";
 
+var args = {
+	usedKeywords: usedKeywords,
+	searchUrl: searchUrl,
+	postUrl: postUrl
+};
+
 describe( "checks for keyword doubles", function(){
 	it("returns array with keyword", function(){
-		var plugin = new PreviouslyUsedKeywords( app, usedKeywords, searchUrl, postUrl );
+		var plugin = new PreviouslyUsedKeywords( app, args, i18n );
 
 		expect( plugin.scoreAssessment( 1, 1, "keyword" ).score ).toBe( 6 );
 		expect( plugin.scoreAssessment( 1, 1, "keyword" ).text ).toBe( "You've used this focus keyword <a href='http://post?1'>once before</a>, " +
@@ -27,6 +32,15 @@ describe( "checks for keyword doubles", function(){
 
 		expect( plugin.scoreAssessment( 0, 0, "bla").score ).toBe( 9 );
 		expect( plugin.scoreAssessment( 0, 0, "bla").text ).toBe( "You've never used this focus keyword before, very good." );
+	});
+});
 
+describe( "replaces keyword usage", function(){
+	it("adds keywords", function(){
+		usedKeywords = {};
+		plugin = new PreviouslyUsedKeywords( app, usedKeywords, searchUrl, postUrl );
+		expect( plugin.usedKeywords ).not.toBeDefined();
+		plugin.updateKeywordUsage(  { "keyword": [1], "test": [2,3,4] } );
+		expect( plugin.usedKeywords[ "keyword" ] ).toContain( 1 );
 	});
 });
