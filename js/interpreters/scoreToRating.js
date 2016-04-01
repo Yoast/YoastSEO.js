99b738f2beb36dc16c7c7beccfac85dbb9461ab2
Anton Timmermans <anton@yoast.com>
2016-04-01 21:47:09 +0200

    Remove erroneous JSDoc

--- js/interpreters/scoreToRating.js
+++ js/interpreters/scoreToRating.js
@@ -1,8 +1,8 @@
 /**
  * Interpreters a score and gives it a particular rating.
+ *
  * @param {Number} score The score to interpreter.
  * @returns {string} The rating, given based on the score.
- * @constructor
  */
 var ScoreToRating = function( score ) {
 
