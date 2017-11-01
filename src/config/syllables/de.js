3de5b340e6b42117fe0dd830bcce4b0dfee4663b
Alexander Botteram <alexander@yoast.com>
2017-11-01 09:09:20 +0100

    Changed JSON files to JS files in src

--- src/config/syllables/de.js
+++ src/config/syllables/de.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
 	"vowels": "aeiouyäöüáéâàèîêâûôœ",
 	"deviations": {
 		"vowels": [
--- src/config/syllables/nl.js
+++ src/config/syllables/nl.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
 	"vowels": "aáäâeéëêiíïîoóöôuúüûy",
 	"deviations": {
 		"vowels": [
