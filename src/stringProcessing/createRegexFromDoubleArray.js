f7d0609c781fb30fc2a539d1ab648bf8e3d299ae
Carlo Cannas <carlo94@gmail.com>
2018-10-22 13:08:22 +0200

    Optimize dynamically created regex
    There's no need to add a word boundary pattern for each word in a disjunction
    list: let's add the word boundary pattern once to the whole regex.
    
    The regex currently created in createRegexFromArray.js can be really huge:
    https://github.com/Yoast/YoastSEO.js/blob/v1.41.0/src/stringProcessing/directPrecedenceException.js#L45
    creates a regex 67840 characters long.
    
    That specific regex also make Chrome 70 crash as soon as the exec method is
    called. It looks like a Chrome regression, since Chrome 69 handles it fine.

--- src/stringProcessing/createRegexFromDoubleArray.js
+++ src/stringProcessing/createRegexFromDoubleArray.js
@@ -1,18 +1,7 @@
 /** @module stringProcessing/createRegexFromDoubleArray */
 
 import addWordBoundary from "../stringProcessing/addWordboundary.js";
-
-/**
- * Creates a regex string of combined strings from the input array.
- * @param {array} array The array containing the various parts of a transition word combination.
- * @returns {array} The array with replaced entries.
- */
-var wordCombinationToRegexString = function( array ) {
-	array = array.map( function( word ) {
-		return addWordBoundary( word );
-	} );
-	return array.join( "(.*?)" );
-};
+import { forEach } from "lodash-es";
 
 /**
  * Creates a regex of combined strings from the input array, containing arrays with two entries.
@@ -20,9 +9,13 @@ var wordCombinationToRegexString = function( array ) {
  * @returns {RegExp} The regex created from the array.
  */
 export default function( array ) {
-	array = array.map( function( wordCombination ) {
-		return wordCombinationToRegexString( wordCombination );
+	const first = [];
+	const second = [];
+	forEach( array, ( [ a, b ] ) => {
+		first.push( a );
+		second.push( b );
 	} );
-	var regexString = "(" + array.join( ")|(" ) + ")";
+	const regexString = addWordBoundary( "(" + first.join( "|" ) + ")" ) +
+		".*?" + addWordBoundary( "(" + second.join( "|" ) + ")" );
 	return new RegExp( regexString, "ig" );
 }
