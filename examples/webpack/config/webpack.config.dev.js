f568447854a6a0471025643b0926fc55e9655f14
Hans-Christiaan <hans-christiaan@yoast.com>
2019-01-08 15:34:50 +0100

    Removed superfluous polyfills (now supplied by babel-polyfill).

--- examples/webpack/config/webpack.config.dev.js
+++ examples/webpack/config/webpack.config.dev.js
@@ -32,9 +32,8 @@ module.exports = {
 	// This means they will be the "root" imports that are included in JS bundle.
 	// The first two entry points enable "hot" CSS and auto-refreshes for JS.
 	entry: [
+		// This fixes some issues when using the example in Internet Explorer 11 (e.g. using 'startsWith' on a string).
 		"babel-polyfill",
-		// We ship a few polyfills by default:
-		require.resolve( './polyfills' ),
 		// Include an alternative client for WebpackDevServer. A client's job is to
 		// connect to WebpackDevServer by a socket and get notified about changes.
 		// When you save a file, the client will either apply hot updates (in case
