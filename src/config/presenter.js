4db0a0105feb2b808841fd48cecaf069a373e28c
Rami Yushuvaev <r_a_m_i@hotmail.com>
2019-05-07 11:11:55 +0300

    i18n: split translation strings to reduce the number of strings

--- src/config/presenter.js
+++ src/config/presenter.js
@@ -8,25 +8,25 @@ export default function( i18n ) {
 		feedback: {
 			className: "na",
 			screenReaderText: i18n.dgettext( "js-text-analysis", "Feedback" ),
-			fullText: i18n.dgettext( "js-text-analysis", "Content optimization: Has feedback" ),
+			fullText: i18n.dgettext( "js-text-analysis", "Content optimization:" ) + ' ' + i18n.dgettext( "js-text-analysis", "Has feedback" ),
 			screenReaderReadabilityText: "",
 		},
 		bad: {
 			className: "bad",
 			screenReaderText: i18n.dgettext( "js-text-analysis", "Needs improvement" ),
-			fullText: i18n.dgettext( "js-text-analysis", "Content optimization: Needs improvement" ),
+			fullText: i18n.dgettext( "js-text-analysis", "Content optimization:" ) + ' ' + i18n.dgettext( "js-text-analysis", "Needs improvement" ),
 			screenReaderReadabilityText: i18n.dgettext( "js-text-analysis", "Needs improvement" ),
 		},
 		ok: {
 			className: "ok",
 			screenReaderText: i18n.dgettext( "js-text-analysis", "OK SEO score" ),
-			fullText: i18n.dgettext( "js-text-analysis", "Content optimization: OK SEO score" ),
+			fullText: i18n.dgettext( "js-text-analysis", "Content optimization:" ) + ' ' + i18n.dgettext( "js-text-analysis", "OK SEO score" ),
 			screenReaderReadabilityText: i18n.dgettext( "js-text-analysis", "OK" ),
 		},
 		good: {
 			className: "good",
 			screenReaderText: i18n.dgettext( "js-text-analysis", "Good SEO score" ),
-			fullText: i18n.dgettext( "js-text-analysis", "Content optimization: Good SEO score" ),
+			fullText: i18n.dgettext( "js-text-analysis", "Content optimization:" ) + ' ' + i18n.dgettext( "js-text-analysis", "Good SEO score" ),
 			screenReaderReadabilityText: i18n.dgettext( "js-text-analysis", "Good" ),
 		},
 	};
